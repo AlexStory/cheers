f90e27dd50b965bbbe98f08c0e317df016a22efd
Alex Story <astory11@yahoo.com>
2014-11-06 13:05:16 -0600

    refactored

---
+++ main.js
@@ -0,0 +1,38 @@
+$('document').ready(function(){
+  $("input[type='button']").click(function(){
+    cheer.getName($('.your_name').val());
+    cheer.appendDom(cheer.name);
+  });
+});
+
+ var cheer = {
+  name: '',
+
+  getName: function(name){
+    cheer.name = name;
+  },
+
+  preamble: function(){
+    return 'Your name is ' + cheer.name
+  },
+
+  postamble: function(){
+    return cheer.name + ' is Great!!!'
+  },
+
+  appendDom: function(){
+    $('ul').empty();
+    $('ul').append($('<li>' + cheer.preamble() +'</li>'));
+    var arrayName = cheer.name.toUpperCase().split('');
+
+    arrayName.forEach(function(letter){
+      if(letter === 'A' ||letter === 'E' ||letter ==='F' ||letter === 'H' ||letter === 'I' ||letter === 'L' ||letter === 'M' ||letter === 'N' || letter === 'O' ||letter ===  'R' ||letter === 'S' || letter === 'X' ){
+        $('ul').append('<li>Give me an... ' + letter + '</li>');
+      } else {
+        $('ul').append('<li>Give me a... ' + letter + '</li>');
+      }
+    })
+
+    $('ul').append($('<li>' + cheer.postamble() + '</li>'));
+  }
+};
---
+++ tests.js
@@ -0,0 +1,37 @@
+QUnit.test( "Get Name", function( assert ) {
+  cheer.getName('Alex');
+  assert.equal( cheer.name, 'Alex');
+
+  cheer.getName('Eliza');
+  assert.equal( cheer.name, 'Eliza');
+});
+
+QUnit.test( "preamble", function( assert ) {
+  cheer.getName('Bob');
+  assert.equal( cheer.preamble(), 'Your name is Bob');
+
+  cheer.getName('Betty');
+  assert.equal( cheer.preamble(), 'Your name is Betty');
+
+});
+
+QUnit.test( "postamble", function( assert ) {
+  cheer.getName('Jack');
+  assert.equal( cheer.postamble(), 'Jack is Great!!!');
+
+  cheer.getName('Jill');
+  assert.equal( cheer.postamble(), 'Jill is Great!!!');
+
+});
+
+QUnit.test( "Test dom append", function( assert ) {
+  cheer.getName('Alex');
+  cheer.appendDom();
+  var firstLine = $('ul li:first-child' ).text();
+  assert.equal( firstLine, 'Your name is Alex');
+
+  cheer.getName('Autumn');
+  cheer.appendDom();
+  var firstLine = $('ul li:first-child' ).text();
+  assert.equal( firstLine, 'Your name is Autumn');
+});
